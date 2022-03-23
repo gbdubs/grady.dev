82e056d07a120fbed850688eff7af988cc96d537
Grady Ward <grady.b.ward@gmail.com>
2022-03-23 10:46:41 -0600

    s/gradybward.com/grady.dev/g

--- regenerate_projects.sh
+++ regenerate_projects.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-cd ~/Desktop/geebs/projectdata
+cd ~/gbdubs/grady.dev/projectdata
 
 hugo
 
-rm -R ../project/
 
+rm -R ../project/
 mkdir ../project
 mkdir ../project/img
-
 cp -R public/project/* ../project
-
 cp -R public/img/* ../project/img
